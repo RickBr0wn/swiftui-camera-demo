2ee4a410f044d4d4b7ee1be285222a2d42d5fbfa
Rick Brown <ricky.brown.00@gmail.com>
2021-06-12 09:21:19 +0100

    initial commit

--- swiftui-camera-demo/swiftui_camera_demoApp.swift
+++ swiftui-camera-demo/swiftui_camera_demoApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct swiftui_camera_demoApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
