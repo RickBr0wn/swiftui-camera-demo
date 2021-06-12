f6bf8d21541d04ca7aa5e30a298b4ecc76e7be4f
Rick Brown <ricky.brown.00@gmail.com>
2021-06-12 09:20:39 +0100

    Initial Commit

---
+++ swiftui-camera-demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-camera-demo
+//
+//  Created by Rick Brown on 12/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui-camera-demo/swiftui_camera_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_camera_demoApp.swift
+//  swiftui-camera-demo
+//
+//  Created by Rick Brown on 12/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_camera_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
